36a097b6cd5d18e986a164ba3cb7885511f471f3
yaredtt <yaredtekle22@gmail.com>
2023-03-03 11:09:15 +0300

    fix linter errors

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,39 +1,37 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
+const list = document.getElementById('list');
+const LIST = [
+  {
 
+    description: 'desc1',
+    completed: true,
+    index: 0,
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
+  },
 
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
+  {
+    description: 'desc2',
+    completed: true,
+    index: 1,
+  },
 
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+  {
+    description: 'desc3',
+    completed: true,
+    index: 2,
+  },
+];
+const lists = () => {
+  for (let i = 0; i < LIST.length; i += 1) {
+    const text = `<li class="item">
+<div class="ip">
+<i class="fa-regular fa-square"></i>
+<p class = "text">${LIST[i].description} </p>
+</div>
+<i class = "fa fa-trash-o de"</i>
+</li>`;
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    // Add the image to our existing div.
-
-  const myIcon = new Image();
-
-  myIcon.src = Icon;
-
-
-  element.appendChild(myIcon);
-  console.log(Data);
-  console.log(Notes);
-    return element;
+    const position = 'beforeend';
+    list.insertAdjacentHTML(position, text);
   }
-  
-  document.body.appendChild(component());
+};
+window.document.addEventListener('DOMContentLoaded', lists);
